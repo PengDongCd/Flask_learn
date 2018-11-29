de04915187919925110ffdd9308d1b3474c5f85a
PengDongCd <dpusername@163.com>
2018-11-29 20:53:41 +0800

    Initial commit

---
+++ app/route.py
@@ -0,0 +1,31 @@
+from app import app
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Dong Peng'}
+    posts = [
+        {
+            'author': {'username': '刘'},
+            'body': '这是模板模块中的循环例子～1'
+
+        },
+        {
+            'author': {'username': '忠强'},
+            'body': '这是模板模块中的循环例子～2'
+        }
+    ]
+    return render_template('index.html', title='My', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('用户登录的名户名是:{} , 是否记住我:{}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='登 录', form=form)
---
+++ myblog.py
@@ -0,0 +1,6 @@
+import wtf as wtf
+
+from app import app
+
+if __name__ == '__main__':
+    app.run()
