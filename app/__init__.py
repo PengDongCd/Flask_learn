ccadb6d1cf01d1b11010f33beb7e8c339fdd8278
PengDongCd <dpusername@163.com>
2018-12-01 16:24:26 +0800

    Finish step3

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import route
+#build relationship with DB
+db = SQLAlchemy(app)
+#binding app and DB for later usage
+migrate = Migrate(app, db)
+
+from app import route, models
--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 class Config(object):
     SECRET_KEY = 'a9087FFJFF9nnvc2@#$%FSD'
+
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Mysql@2018@localhost:3306/flaskblog?charset=utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
